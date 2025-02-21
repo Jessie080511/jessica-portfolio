c2e89e6b26c1a914e32a8bcab8f2032661fc4f7a
Jessie080511 <yanjie080511@gmail.com>
2025-02-21 14:18:08 -0800

    Clients.jsx

--- src/sections/Clients.jsx
+++ src/sections/Clients.jsx
@@ -20,11 +20,6 @@ const Clients = () => {
                   </div>
                 </div>
 
-                <div className="flex self-end items-center gap-2">
-                  {Array.from({ length: 5 }).map((_, index) => (
-                    <img key={index} src="/assets/star.png" alt="star" className="w-5 h-5" />
-                  ))}
-                </div>
               </div>
             </div>
           </div>
