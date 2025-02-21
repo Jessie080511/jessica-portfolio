24040b12eb14846efed9da6dcf256206aeb5feaf
Jessie080511 <yanjie080511@gmail.com>
2025-02-21 14:14:27 -0800

    index.js

--- src/constants/index.js
+++ src/constants/index.js
@@ -26,14 +26,14 @@ export const clientReviews = [
     id: 1,
     name: 'Southwest University',
     position: 'Bachelor of Science in Accountancy',
-    img: 'assets/review1.png',
+    img: 'assets/Southwest_University.png',
     review:'During my undergraduate studies, I studied finance, financial management, tax law, statistics, and other courses. I also taught myself computer knowledge such as linear algebra, python, calculus, and HTML. Finally, I took a minor in law and learned some international law knowledge.'
   },
   {
     id: 2,
     name: 'San Francisco Bay University',
     position: 'Master of Computer Science',
-    img: 'assets/review2.png',
+    img: 'assets/SFBU.png',
     review:
       'During my postgraduate studies, I took a comprehensive range of computer courses, including Software Quality Assurance and Test Automation, Object Oriented Design in Python, Database Technologies, Cloud Security, Algorithms, Mathematics and Statistics for Data Science, Data Modeling and Implementation Techniques.',
   },
