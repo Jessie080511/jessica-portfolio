013a28cc3831b4ec6cd9b448f9e90927543361bf
Jessie080511 <yanjie080511@gmail.com>
2025-02-21 14:10:29 -0800

    Update index.js

--- src/constants/index.js
+++ src/constants/index.js
@@ -24,35 +24,18 @@ export const navLinks = [
 export const clientReviews = [
   {
     id: 1,
-    name: 'Emily Johnson',
-    position: 'Marketing Director at GreenLeaf',
+    name: 'Southwest University',
+    position: 'Bachelor of Science in Accountancy',
     img: 'assets/review1.png',
-    review:
-      'Working with Adrian was a fantastic experience. He transformed our outdated website into a modern, user-friendly platform. His attention to detail and commitment to quality are unmatched. Highly recommend him for any web dev projects.',
+    review:'During my undergraduate studies, I studied finance, financial management, tax law, statistics, and other courses. I also taught myself computer knowledge such as linear algebra, python, calculus, and HTML. Finally, I took a minor in law and learned some international law knowledge.'
   },
   {
     id: 2,
-    name: 'Mark Rogers',
-    position: 'Founder of TechGear Shop',
+    name: 'San Francisco Bay University',
+    position: 'Master of Computer Science',
     img: 'assets/review2.png',
     review:
-      'Adrian’s expertise in web development is truly impressive. He delivered a robust and scalable solution for our e-commerce site, and our online sales have significantly increased since the launch. He’s a true professional! Fantastic work.',
-  },
-  {
-    id: 3,
-    name: 'John Dohsas',
-    position: 'Project Manager at UrbanTech ',
-    img: 'assets/review3.png',
-    review:
-      'I can’t say enough good things about Adrian. He was able to take our complex project requirements and turn them into a seamless, functional website. His problem-solving abilities are outstanding.',
-  },
-  {
-    id: 4,
-    name: 'Ether Smith',
-    position: 'CEO of BrightStar Enterprises',
-    img: 'assets/review4.png',
-    review:
-      'Adrian was a pleasure to work with. He understood our requirements perfectly and delivered a website that exceeded our expectations. His skills in both frontend backend dev are top-notch.',
+      'During my postgraduate studies, I took a comprehensive range of computer courses, including Software Quality Assurance and Test Automation, Object Oriented Design in Python, Database Technologies, Cloud Security, Algorithms, Mathematics and Statistics for Data Science, Data Modeling and Implementation Techniques.',
   },
 ];
 
