6260d50cd392b5b66c9fab1e5694eda14d2a23bf
Jessie080511 <yanjie080511@gmail.com>
2025-02-26 14:13:46 -0800

    HackerRoom.jsx

--- src/components/HackerRoom.jsx
+++ src/components/HackerRoom.jsx
@@ -19,18 +19,18 @@ export function HackerRoom(props) {
       </mesh>
       <mesh geometry={nodes.screen_glass_glass_0.geometry} material={materials.glass} />
       <mesh geometry={nodes.table_table_mat_0_1.geometry}>
-        <meshStandardMaterial color="#B5C4B1" /> {/* 桌子绿色 */}
+        <meshStandardMaterial color="#D6C8C8" /> {/* 桌子绿色 */}
       </mesh>
-      <mesh geometry={nodes.table_table_mat_0_2.geometry} material={materials.computer_mat}>
-        <meshMatcapMaterial map={monitortxt} />
+      <mesh geometry={nodes.table_table_mat_0_2.geometry}>
+        <meshStandardMaterial color="#F2E9E9" /> {/* 暖白色电脑 */}
       </mesh>
       <mesh geometry={nodes.table_table_mat_0_3.geometry} material={materials.server_mat} />
       <mesh geometry={nodes.table_table_mat_0_4.geometry} material={materials.vhsPlayer_mat} />
       <mesh geometry={nodes.table_table_mat_0_5.geometry} material={materials.stand_mat} />
       <mesh geometry={nodes.table_table_mat_0_6.geometry} material={materials.mat_mat} />
       <mesh geometry={nodes.table_table_mat_0_7.geometry} material={materials.arm_mat} />
-      <mesh geometry={nodes.table_table_mat_0_8.geometry} material={materials.tv_mat}>
-        <meshMatcapMaterial map={monitortxt} />
+      <mesh geometry={nodes.table_table_mat_0_8.geometry}>
+        <meshStandardMaterial color="#C0B3B3" /> {/* 柔和灰粉色显示器 */}
       </mesh>
       <mesh geometry={nodes.table_table_mat_0_9.geometry} material={materials.cables_mat} />
       <mesh geometry={nodes.table_table_mat_0_10.geometry} material={materials.props_mat} />
